7d1f1747c6c235206718a78743b61ad990623e33
Oblivios <68814228+Oblivios@users.noreply.github.com>
2021-05-16 22:42:03 +0200

    Update Crypt.py

--- Crypt.py
+++ Crypt.py
@@ -36,19 +36,45 @@ elif choice == "n":
 print("Key: "+key.decode())
 f = Fernet(key)
 while True:
-    keyboard.wait("ctrl+c")
-    keyboard.wait("ctrl+c")
-    record = pc.paste()
-    if record.startswith("gAAAAA")==True:
-        decrypted = f.decrypt(record.encode())
-        print("decrypted message:")
-        print(decrypted.decode())
-        print("")
-    else:
-        encrypted = f.encrypt(record.encode())
-        print("encrypted message:")
-        print(encrypted.decode())
-        print("")
-        keyboard.press_and_release("ctrl+a")
-        keyboard.press_and_release("del")
-        keyboard.write(str(encrypted.decode()))
+        if keyboard.is_pressed("f2"):
+        print("Waiting for your copy...")
+        keyboard.wait("ctrl+c")
+        keyboard.wait("ctrl+c")
+        record = pc.paste()
+        if record.startswith("gAAAAA")==True:
+            try:
+                decrypted = f.decrypt(record.encode())
+                print("decrypted message:")
+                print(decrypted.decode())
+                print("")
+            except cryptography.exceptions.InvalidSignature:
+                print("Can't decrypt the message.\nYou may have the wrong key!\n")
+            except cryptography.fernet.InvalidToken:
+                print("Can't decrypt the message.\nYou may have the wrong key!\n")
+        else:
+            encrypted = f.encrypt(record.encode())
+            print("encrypted message:")
+            print(encrypted.decode())
+            print("")
+            keyboard.press_and_release("ctrl+a")
+            keyboard.press_and_release("del")
+            keyboard.write(encrypted.decode())
+            time.sleep(0.1)
+            keyboard.press_and_release("enter")
+    elif keyboard.is_pressed("f3"):
+        print("Paused... Press F3 to re-run CryptPy.")
+        keyboard.wait("f3")
+        print("Unpaused!")
+    elif keyboard.is_pressed("f4"):
+        choice = input("Generate Key ? (Y/N) ").lower()
+        if choice == "y":
+            key = Fernet.generate_key()
+        elif choice == "n":
+            key = input("Copy and Paste the key here:").encode()
+        print("Key: "+key.decode())
+        f = Fernet(key)
+    elif keyboard.is_pressed("f5"):
+        leave = str(input("Type any key to leave... type C to cancel...")).lower()
+        if leave!="c":
+            exit()
+    time.sleep(0.2)
